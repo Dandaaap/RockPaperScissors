ece5dcc3fadaef800e8127a68b2b83b7b00765a0
Dandaaap <dan.peterek@gmail.com>
2025-04-02 16:25:38 +0200

    Add round function with if blocks

--- script.js
+++ script.js
@@ -14,14 +14,32 @@ function getComputerChoice() {
   } else {
     randomChoice = "scissors";
   }
+  console.log(randomChoice);
   return randomChoice;
 }
-
-console.log(getComputerChoice());
 
 function getHumanChoice() {
   let randomChoice = prompt("What will you play?");
   return randomChoice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log("Its a tie!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
